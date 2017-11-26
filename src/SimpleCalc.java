b05b2b126595bb1e56409f0947f593cfa0f61573
Nratsiburinskiy <0954634354n@gmail.com>
2017-11-27 00:15:23 +0200

    Input debug.

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -6,11 +6,24 @@ public class SimpleCalc
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Please input first number: ");
-        double a = in.nextDouble();
-        System.out.println("Your input "+a +" as a first number.\n");
-        System.out.println("Please input second number: \n");
-        double b = in.nextDouble();
-        System.out.println("Your input "+b +" as a second number.\n");
+        double a = 0;double k=0;
+        while (k==0)
+        {
+            if (!in.hasNextDouble())  {System.out.println("Input is not correct,try harder");in = new Scanner(System.in);}
+            else {a = in.nextDouble();k=1;
+            System.out.println("Your input " + a + " as a first number.");}
+        }
+
+        System.out.println("Please input second number: ");
+        double b = 0; k=0;
+        while (k==0)
+        {
+            if (!in.hasNextDouble())  {System.out.println("Input is not correct,try harder");in = new Scanner(System.in);}
+            else
+                {b = in.nextDouble(); if (b!=0)
+            {k=1; System.out.println("Your input " + b + " as a second number.");}
+                else System.out.println("Second number can't be 0");}
+        }
         System.out.println("Your");
         double sum = a+b;
         System.out.println("Sum is "+sum);
